1cd820e5a0d4aa7d146736f928185f368c8d3dbb
Runald D'souza <31964132+runalddsouza@users.noreply.github.com>
2021-12-29 23:43:43 +0530

    remove spi table job

--- scripts/create_delta_tables.sh
+++ scripts/create_delta_tables.sh
@@ -9,19 +9,6 @@ spark-submit --packages io.delta:delta-core_2.12:1.1.0 \
 --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" \
 --master "local[*]" \
 --py-files "$PY_FILES" \
-"$PY_JOB_MODULE_PATH/spi_build_table.py" \
---partitionkeys 10 \
---partitions 5 \
---key primary_key \
---input "https://projects.fivethirtyeight.com/soccer-api/club/spi_matches_latest.csv" \
---outputprefix "$OUTPUT_LOCATION" \
---table spi_matches_latest
-
-spark-submit --packages io.delta:delta-core_2.12:1.1.0 \
---conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
---conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" \
---master "local[*]" \
---py-files "$PY_FILES" \
 "$PY_JOB_MODULE_PATH/pricing_load.py" \
 --partitionkeys 10 \
 --partitions 5 \
